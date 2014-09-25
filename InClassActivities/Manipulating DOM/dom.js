d45326f9fa63eaaba7f5e6cfefb74ae8933330cb
afreeland <afreeland@gmail.com>
2014-09-25 19:07:24 -0400

    JS Updates

--- InClassActivities/Manipulating DOM/dom.js
+++ InClassActivities/Manipulating DOM/dom.js
@@ -44,3 +44,37 @@ function init () {
 
 	_body.innerHTML = html;
 }
+
+function add () {
+	var _input = document.getElementById('myInput');
+	console.log('get input', _input);
+
+
+	var _value = _input.value;
+	console.log('actual value', _value);
+
+	var SplitValue = _value.split(',');
+	console.log('Our split', SplitValue);
+
+	// Validate age
+	if(isNaN(SplitValue[3])){
+		alert(SplitValue[0] + ' ' + SplitValue[1] + 'not valid age');
+		return;
+	}
+		
+
+	var newContact = {
+		firstName: SplitValue[0],
+		lastName: SplitValue[1],
+		email: SplitValue[2],
+		age: SplitValue[3]
+	};
+
+	console.log('Our new contact', newContact);
+
+	contacts.push(newContact);
+	console.log('updated contacts');
+
+	init();
+	console.log('re ran init');
+}
