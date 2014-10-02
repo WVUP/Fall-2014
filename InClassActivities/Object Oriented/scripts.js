5e5d926ff5e666ee32cb5d4cf87f9cc9a58bc9e6
afreeland <afreeland@gmail.com>
2014-10-02 19:05:34 -0400

    final update with ul sub list

--- InClassActivities/Object Oriented/scripts.js
+++ InClassActivities/Object Oriented/scripts.js
@@ -4,10 +4,42 @@ var arr = ['Aaron', 'Pepper', 'Chris'];
 // arr.splice(startIndex, howMany)
 // arr.pop()
 
+
+function accessGlobal () {
+	var k = arr;
+	console.log(k);
+}
+
+function testScope () {
+	// internal local variable arr, which is NOT the same
+	// as the global arr variable on line 1
+	var arr = [1, 2, 3];
+	console.log(arr);
+	
+	//window.arr = window.arr.concat(arr);
+	window.arr = arr;
+	console.log(window.arr);
+}
+
 var contact = {
 	firstName: 'Aaron',
 	lastName: 'Freeland',
 	email: 'afreeland@gmail.com',
+	languages: [
+		{
+			name: 'C#',
+			label: 'C Sharp'
+		},
+		{
+			name: 'JS',
+			label: 'JavaScript'
+		}
+	],
+	drinks: {
+		pepsi: ['Mtn. Dew', 'Voltage'],
+		coke: ['Dr.Pepper'],
+		'dr pepper': ['cherry', 'vanilla']
+	},
 	alert: function () {
 		var combined = this.firstName + ' ' + this.lastName;
 		console.log(combined);
@@ -26,10 +58,15 @@ function customAlert(obj){
 }
 
 
-function Contact (fName, lName) {
+function Contact (fName, lName, age) {
 	this.firstName = fName;
 	this.lastName = lName;
+	this.age = age || null;
 
 	var combined = this.firstName + ' ' + this.lastName;
 	this.fullName = combined;
 }
+
+Contact.prototype.alert = function () {
+	alert(this.fullName);
+};
