9fd660e191f8a2ac55dfe84de14bb2ce7c72abb7
EnWallis <EnWallis@gmail.com>
2014-10-16 16:33:37 -0400

    Meh

--- Assignments/DConner/SadSadPlaceholder/FormanGriddle/MYBRAND/INDEX_SCRIPT.js
+++ Assignments/DConner/SadSadPlaceholder/FormanGriddle/MYBRAND/INDEX_SCRIPT.js
@@ -1,4 +1,9 @@
-function charON( charname ){
-   
+function charON(charname){
+   vee = document.getElementById(charname);
+   vee.style.display = "block";
+}
 
+function charOFF(charname){
+   vee = document.getElementById(charname);
+   vee.style.display = "none";
 }
