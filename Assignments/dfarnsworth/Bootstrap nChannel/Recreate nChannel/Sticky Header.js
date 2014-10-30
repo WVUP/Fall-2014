caa039f77bd2bfe69b80a5e3aa19eb177064e5f4
DustinFarnsworth <dfarnswo@wvup.edu>
2014-10-30 10:38:51 -0400

    nChannel scrolling logo shell
    Need to write the JS to make image scroll on and off the page.

--- Assignments/dfarnsworth/Bootstrap nChannel/Recreate nChannel/Sticky Header.js
+++ Assignments/dfarnsworth/Bootstrap nChannel/Recreate nChannel/Sticky Header.js
@@ -9,15 +9,6 @@ if ($(this).scrollTop() > 1){
 
 $(window).scroll(function() {
 if ($(this).scrollTop() > 1){  
-    $('#poof').addClass("show");
-  }
-  else{
-    $('#poof').removeClass("show");
-  }
-});
-
-$(window).scroll(function() {
-if ($(this).scrollTop() > 1){  
     $('#np').addClass("lpad");
   }
   else{
@@ -54,6 +45,15 @@ if ($(this).scrollTop() > 1){
 
 $(window).scroll(function() {
 if ($(this).scrollTop() > 1){  
+    $('#poof').addClass("show");
+  }
+  else{
+    $('#poof').removeClass("show");
+  }
+});
+
+$(window).scroll(function() {
+if ($(this).scrollTop() > 1){  
     $('#index').addClass("implode");
   }
   else{
@@ -83,3 +83,20 @@ function preheader(){
   $('#hbtn').addClass("btop");
   $('#np').addClass("pad");
 };
+
+//1st scrollbar range 800-1300:
+//2nd scrollbar range 2060-2560:
+/*
+$(window).scroll(function() {
+if ($(this).scrollTop() > 800 and ($(this).scrollTop() < 1300){  
+    $x = -800;
+    $margin = -1180;
+    $x += (this.scrolltop();
+    $margin += 2 * x;
+//Insert margin value into offscreen image margin:
+  }
+  else{
+    $margin = -1180;
+  }
+});
+*/
